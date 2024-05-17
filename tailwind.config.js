f514083ab6ae8f2a21f59cfb09e500115ce527b7
Kishore Kumaran <kishore.kumaran04@gmail.com>
2024-05-17 00:42:04 -0400

    adding google fonts, customising tailwind theme and adding constants to mock api data

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "white",
+        none: "none"
+      },
+      borderWidth: {
+        1: "1px"
+      },
+      fontFamily: {
+        quickSand: ["Quicksand", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
